9759213ad62c99456eefde5c050779e58f8e2754
Bhumika-Kumar <153841368+Bhumika-Kumar@users.noreply.github.com>
2026-08-24 11:51:22 +0530

    Time: 1 ms (99.94%), Space: 46.8 MB (65.28%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,22 +1,30 @@
 class Solution {
     public int singleNumber(int[] nums) {
         // this approach is just reference for hashmap but in this program they said constant space so we can't use hashmap
-        HashMap<Integer, Integer> map=new HashMap<>();
-        for(int num:nums){
-            // if(map.containsKey(num)){
-            //     int freq=map.get(num);
-            //     map.put(num,freq+1);
-            // } else{
-            //     map.put(num,1);
-            // }
+        // HashMap<Integer, Integer> map=new HashMap<>();
+        // for(int num:nums){
+        //     // if(map.containsKey(num)){
+        //     //     int freq=map.get(num);
+        //     //     map.put(num,freq+1);
+        //     // } else{
+        //     //     map.put(num,1);
+        //     // }
 
-           map.put(num, map.getOrDefault(num,0)+1);
+        //    map.put(num, map.getOrDefault(num,0)+1);
+        // }
+        // for(Map.Entry <Integer,Integer> entry : map.entrySet()){
+        //     if(entry.getValue()==1){
+        //         return entry.getKey();
+        //     }
+        // }
+        // return -1;
+
+        // main methor XOR
+        // any number xor the same number cancels each other
+        int res=0;
+        for (int num:nums){
+            res^=num;
         }
-        for(Map.Entry <Integer,Integer> entry : map.entrySet()){
-            if(entry.getValue()==1){
-                return entry.getKey();
-            }
-        }
-        return -1;
+        return res;
     }
 }
