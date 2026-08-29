8b8f45861e588d6644ee51438b12f78a8c630f55
Bhumika-Kumar <153841368+Bhumika-Kumar@users.noreply.github.com>
2026-08-29 11:58:16 +0530

    Time: 2 ms (92.95%), Space: 20.5 MB (72.79%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,19 +3,34 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        m,n=len(matrix) , len(matrix[0])
-        rows=set()
-        cols=set()
+        # m,n=len(matrix) , len(matrix[0])
+        # rows=set()
+        # cols=set()
 
-        for i in range(m):
-            for j in range(n):
-                if matrix[i][j]==0:
-                    rows.add(i)
-                    cols.add(j)
-        for row in rows:
-            for j in range(n):
-                matrix[row][j]=0
-        for col in cols:
-            for j in range(m):
-                matrix[j][col]=0
+        # for i in range(m):
+        #     for j in range(n):
+        #         if matrix[i][j]==0:
+        #             rows.add(i)
+        #             cols.add(j)
+        # for row in rows:
+        #     for j in range(n):
+        #         matrix[row][j]=0
+        # for col in cols:
+        #     for j in range(m):
+        #         matrix[j][col]=0
+
+        m = len(matrix) 
+        n = len(matrix[0])
+        rows = [False] * m 
+        cols = [False] * n  
+        for i in range(m): 
+            for j in range(n): 
+                if matrix[i][j] == 0: 
+                    rows[i] = True
+                    cols[j] = True 
+        # Second loop: set the required rows and columns to zero 
+        for i in range(m): 
+            for j in range(n): 
+                if rows[i] or cols[j]: 
+                    matrix[i][j] = 0
         
