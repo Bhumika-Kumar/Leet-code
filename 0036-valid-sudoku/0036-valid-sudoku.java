61c76075bea558fff4fa81562151072f1547945a
Bhumika-Kumar <153841368+Bhumika-Kumar@users.noreply.github.com>
2026-08-29 10:19:39 +0530

    Time: 1 ms (99.93%), Space: 46 MB (97.91%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,21 +1,43 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashMap<String,Integer> map=new HashMap<>();
+        // Hashmap give 14 ms where this approach give 1 ms but see both ways
+        boolean[][] row=new boolean[9][9];
+        boolean[][] col=new boolean[9][9];
+        boolean[][] box=new boolean[9][9];
+
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
                 if(board[i][j]!='.'){
-                    String row=board[i][j]+"row: "+i;
-                    String col=board[i][j]+"col: "+j;
-                    String box=board[i][j]+"box: "+(i/3)+"and"+(j/3);
+                    int num=board[i][j] - '1';
+                    int boxnumber=(i/3)*3 + (j/3);
 
-                    map.put(row,map.getOrDefault(row,0)+1);
-                    map.put(col,map.getOrDefault(col,0)+1);
-                    map.put(box,map.getOrDefault(box,0)+1);
+                    if(row[i][num] || col[j][num] || box[boxnumber][num]){
+                        return false;
+                    }
 
-                    if(map.get(row)>1 || map.get(col)>1 || map.get(box)>1) return false;
+                    row[i][num] = col[j][num] = box[boxnumber][num] = true;
                 }
             }
         }
         return true;
+
+
+        // HashMap<String,Integer> map=new HashMap<>();
+        // for(int i=0;i<9;i++){
+        //     for(int j=0;j<9;j++){
+        //         if(board[i][j]!='.'){
+        //             String row=board[i][j]+"row: "+i;
+        //             String col=board[i][j]+"col: "+j;
+        //             String box=board[i][j]+"box: "+(i/3)+"and"+(j/3);
+
+        //             map.put(row,map.getOrDefault(row,0)+1);
+        //             map.put(col,map.getOrDefault(col,0)+1);
+        //             map.put(box,map.getOrDefault(box,0)+1);
+
+        //             if(map.get(row)>1 || map.get(col)>1 || map.get(box)>1) return false;
+        //         }
+        //     }
+        // }
+        // return true;
     }
 }
