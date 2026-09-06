23a0175cd3064f61468ba6c2d66ebe798c104bd8
Bhumika-Kumar <153841368+Bhumika-Kumar@users.noreply.github.com>
2026-09-06 15:56:22 +0530

    Time: 0 ms (100%), Space: 47.3 MB (48.86%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,22 +1,33 @@
 public class Solution {
     public int missingNumber(int[] nums) {
-        int i=0;
-        while(i<nums.length){
-            int correct=nums[i];
-            if(nums[i]<nums.length && nums[i]!=nums[correct]){
-                int temp=nums[i];
-                nums[i]=nums[correct];
-                nums[correct]=temp;
-            }
-            else{
-                i++;
-            }
+        //cyclic sort
+        
+        // int i=0;
+        // while(i<nums.length){
+        //     int correct=nums[i];
+        //     if(nums[i]<nums.length && nums[i]!=nums[correct]){
+        //         int temp=nums[i];
+        //         nums[i]=nums[correct];
+        //         nums[correct]=temp;
+        //     }
+        //     else{
+        //         i++;
+        //     }
+        // }
+        // for(int index=0;index<nums.length;index++){
+        //     if(nums[index]!=index){
+        //         return index;
+        //     }
+        // }
+        // return nums.length;
+
+        int missing = nums.length;
+        
+        for (int i = 0; i < nums.length; i++) {
+            missing ^= nums[i] ^ i;
         }
-        for(int index=0;index<nums.length;index++){
-            if(nums[index]!=index){
-                return index;
-            }
-        }
-        return nums.length;
+        
+        return missing;
+
     }
 }
