edeb333ce03ceb04351342008ec64fc7d084f563
Bhumika-Kumar <153841368+Bhumika-Kumar@users.noreply.github.com>
2026-09-13 08:40:03 +0530

    Time: 35 ms (41.04%), Space: 59.3 MB (32.19%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,31 +1,31 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        int n = nums.length;
-        List<List<Integer>> ans = new ArrayList<>();
-
-        for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            int left = i + 1, right = n - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    left++;
-                    right--;
-                    while (left < right && nums[left] == nums[left - 1]) left++;
-                    while (left < right && nums[right] == nums[right + 1]) right--;
-                } else if (sum < 0) {
-                    left++;
-                } else {
-                    right--;
+        int n=nums.length;
+        List<List<Integer>> res=new ArrayList<>();
+        for(int i=0;i<n-1;i++){
+            if(i==0 || i>0 && nums[i]!=nums[i-1]){
+                int low=i+1, high=n-1,target=-nums[i];
+                while(low<high){
+                    int currentvalue=nums[low]+nums[high];
+                    if(currentvalue==target){
+                        res.add(Arrays.asList(nums[i],nums[low],nums[high]));
+                    while(low<high && nums[low]==nums[low+1]) low++;
+                    while(low<high && nums[high]==nums[high-1]) high--;
+                    low++;
+                    high--;
+                    }
+                    else if(currentvalue>target){
+                        high--;
+                    }
+                    else{
+                        low++;
+                    }
+                    
                 }
             }
         }
-
-        return ans;
+        return res;
+       
     }
 }
